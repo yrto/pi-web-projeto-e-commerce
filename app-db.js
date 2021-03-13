9a0b12470f6462fac482bcc5ee4e3eff386a1c08
Ayrton Mugnaini <ayrton.mugnaini@gmail.com>
2021-03-12 22:52:52 -0300

    Primeiras funcionalidades de renderização de produtos

---
+++ app-db.js
@@ -0,0 +1,37 @@
+let database = [
+    {
+        "id": "0001",
+        "name": "Produto A",
+        "img": "images/0001.jpg",
+        "description": "Descrição do produto A",
+        "category": "comida",
+        "stock": 10,
+        "price": 9.50,
+        "discount": 0.10,
+        "featured": true
+    },
+    {
+        "id": "0002",
+        "name": "Produto B",
+        "img": "images/0002.jpg",
+        "description": "Descrição do produto B",
+        "category": "comida",
+        "stock": 10,
+        "price": 9.50,
+        "discount": 0.10,
+        "featured": true
+    },
+    {
+        "id": "0003",
+        "name": "Produto C",
+        "img": "images/0003.jpg",
+        "description": "Descrição do produto B",
+        "category": "bebida",
+        "stock": 10,
+        "price": 9.50,
+        "discount": 0.25,
+        "featured": true
+    }
+]
+
+export default database
---
+++ app-main.js
@@ -0,0 +1,65 @@
+import database from "./app-db.js"
+
+//
+// - - - - - - - - - - - - - - - - - - - - help
+//
+
+const log = something => console.log(something)
+
+//
+// - - - - - - - - - - - - - - - - - - - - locations
+//
+
+const productsSection = document.querySelector("#products section ul")
+
+//
+// - - - - - - - - - - - - - - - - - - - - classes
+//
+
+class ProductList {
+    constructor(data) {
+        this.products = data.map(product => product = new Product(product))
+    }
+    renderProducts(list, location) {
+        list.map(product => location.appendChild(product.renderSingleProduct()))
+    }
+    filterProducts(category) {
+        return this.products.filter(product => product.category === category)
+    }
+}
+
+class Product {
+    constructor(product) {
+        this.id = product.id
+        this.name = product.name
+        this.img = product.img
+        this.description = product.description
+        this.category = product.category
+        this.stock = product.stock
+        this.price = product.price
+    }
+    renderSingleProduct() {
+        const imageElement = document.createElement("img")
+        imageElement.setAttribute("src", this.img)
+        imageElement.setAttribute("alt", this.name)
+        const descriptionElement = document.createElement("p")
+        descriptionElement.appendChild(document.createTextNode(this.name))
+        const productElement = document.createElement("li")
+        productElement.appendChild(imageElement)
+        productElement.appendChild(descriptionElement)
+        return productElement
+    }
+}
+
+//
+// - - - - - - - - - - - - - - - - - - - - tests
+//
+
+const db = new ProductList(database)
+
+db.renderProducts(db.products, productsSection)
+
+log(typeof db)
+
+log(db.filterProducts("bebida")[0].renderSingleProduct())
+log(productsSection)
